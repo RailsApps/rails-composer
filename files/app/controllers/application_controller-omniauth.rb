41c7004feea81a38052ed713b25e3851e539d5e1
Daniel Kehoe <daniel@danielkehoe.com>
2012-11-08 16:26:30 -0800

    add ApplicationController file used by rails_apps_composer to create an OmniAuth app

---
+++ files/app/controllers/application_controller-omniauth.rb
@@ -0,0 +1,33 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  helper_method :current_user
+  helper_method :user_signed_in?
+  helper_method :correct_user?
+
+  private
+    def current_user
+      begin
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      rescue Exception => e
+        nil
+      end
+    end
+
+    def user_signed_in?
+      return true if current_user
+    end
+
+    def correct_user?
+      @user = User.find(params[:id])
+      unless current_user == @user
+        redirect_to root_url, :alert => "Access denied."
+      end
+    end
+
+    def authenticate_user!
+      if !current_user
+        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+      end
+    end
+
+end
