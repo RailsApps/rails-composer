d4fcdb57ff302d6dbbc572b27bdf398dab5e6c69
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-08 10:21:17 -0800

    add 'Base Errors Helper for SimpleForm'

---
+++ files/app/helpers/application_helper-simple_form.rb
@@ -0,0 +1,15 @@
+module ApplicationHelper
+  
+  def display_base_errors resource
+    return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
+    messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-block">
+      <button type="button" class="close" data-dismiss="alert">&#215;</button>
+      #{messages}
+    </div>
+    HTML
+    html.html_safe
+  end
+  
+end
