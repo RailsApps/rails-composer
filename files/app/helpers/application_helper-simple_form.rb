28855c7a55a2773188d03f5e40d2d35efe35fb28
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-22 11:10:58 -0800

    remove trailing whitespace and add final newline

--- files/app/helpers/application_helper-simple_form.rb
+++ files/app/helpers/application_helper-simple_form.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   def display_base_errors resource
     return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
     messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
@@ -11,5 +11,5 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
-  
+
 end
