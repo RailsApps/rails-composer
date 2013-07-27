1db10750c1932c91dfc8f8543859379bd7e0ef85
Daniel Kehoe <daniel@danielkehoe.com>
2013-07-27 15:05:26 -0700

    add RegistrationsController for use with Devise and Rails 4

---
+++ files/app/controllers/registrations_controller-devise.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_filter :update_sanitized_params, if: :devise_controller?
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
+  end
+
+end
