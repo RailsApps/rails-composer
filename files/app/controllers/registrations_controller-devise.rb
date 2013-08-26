673557321a589ee4f5e49e49ef14e91b40095e74
Daniel Kehoe <daniel@danielkehoe.com>
2013-08-26 13:49:07 -0700

    fix failure to update username with Rails 4 and Devise (closes #119)

--- files/app/controllers/registrations_controller-devise.rb
+++ files/app/controllers/registrations_controller-devise.rb
@@ -3,6 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
   end
 
 end
