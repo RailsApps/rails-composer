6367d6e59bbebf2c38fea564df10db982d064571
Daniel Kehoe <daniel@danielkehoe.com>
2014-06-30 19:18:47 -0700

    build using rails_apps_composer 3.0.5

--- composer.rb
+++ composer.rb
@@ -93,7 +93,7 @@ module Gemfile
 end
 def add_gem(*all) Gemfile.add(*all); end
 
-@recipes = ["core", "git", "railsapps", "learn_rails", "rails_bootstrap", "rails_foundation", "rails_omniauth", "rails_devise", "rails_devise_pundit", "rails_signup_download", "setup", "locale", "readme", "gems", "tests", "email", "devise", "omniauth", "roles", "frontend", "pages", "init", "extras", "deployment"]
+@recipes = ["core", "git", "railsapps", "learn_rails", "rails_bootstrap", "rails_foundation", "rails_omniauth", "rails_devise", "rails_devise_pundit", "rails_signup_download", "rails_mailinglist_signup", "setup", "locale", "readme", "gems", "tests", "email", "devise", "omniauth", "roles", "frontend", "pages", "init", "extras", "deployment"]
 @prefs = {}
 @gems = []
 @diagnostics_recipes = [["example"], ["setup"], ["railsapps"], ["gems", "setup"], ["gems", "readme", "setup"], ["extras", "gems", "readme", "setup"], ["example", "git"], ["git", "setup"], ["git", "railsapps"], ["gems", "git", "setup"], ["gems", "git", "readme", "setup"], ["extras", "gems", "git", "readme", "setup"], ["email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["core", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["core", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["core", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["email", "example", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["email", "example", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["email", "example", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["apps4", "core", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["apps4", "core", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "tests"], ["apps4", "core", "deployment", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "testing"], ["apps4", "core", "deployment", "email", "extras", "frontend", "gems", "git", "init", "railsapps", "readme", "setup", "tests"], ["apps4", "core", "deployment", "devise", "email", "extras", "frontend", "gems", "git", "init", "omniauth", "pundit", "railsapps", "readme", "setup", "tests"]]
@@ -365,6 +365,7 @@ when "4"
         [["learn-rails", "learn-rails"],
         ["rails-bootstrap", "rails-bootstrap"],
         ["rails-foundation", "rails-foundation"],
+        ["rails-mailinglist-signup", "rails-mailinglist-signup"],
         ["rails-omniauth", "rails-omniauth"],
         ["rails-devise", "rails-devise"],
         ["rails-devise-pundit", "rails-devise-pundit"],
@@ -658,6 +659,81 @@ if prefer :apps4, 'rails-signup-download'
   end
 end
 # >-------------------- recipes/rails_signup_download.rb ---------------------end<
+# >-------------------------- templates/recipe.erb ---------------------------end<
+
+# >-------------------------- templates/recipe.erb ---------------------------start<
+# >-----------------------[ rails_mailinglist_signup ]------------------------<
+@current_recipe = "rails_mailinglist_signup"
+@before_configs["rails_mailinglist_signup"].call if @before_configs["rails_mailinglist_signup"]
+say_recipe 'rails_mailinglist_signup'
+@configs[@current_recipe] = config
+# >------------------- recipes/rails_mailinglist_signup.rb -------------------start<
+
+# Application template recipe for the rails_apps_composer. Change the recipe here:
+# https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/rails_mailinglist_signup.rb
+
+if prefer :apps4, 'rails-mailinglist-signup'
+  prefs[:authentication] = false
+  prefs[:authorization] = false
+  prefs[:better_errors] = true
+  prefs[:deployment] = 'none'
+  prefs[:devise_modules] = false
+  prefs[:form_builder] = 'simple_form'
+  prefs[:git] = true
+  prefs[:local_env_file] = false
+  prefs[:pry] = false
+  prefs[:quiet_assets] = true
+  prefs[:secrets] = ['mailchimp_list_id', 'mailchimp_api_key']
+  prefs[:pages] = 'none'
+  prefs[:locale] = 'none'
+
+  # gems
+  add_gem 'activejob', github: 'rails/activejob'
+  add_gem 'gibbon'
+  add_gem 'sucker_punch'
+
+  stage_two do
+    say_wizard "recipe stage two"
+    generate 'model Visitor email:string'
+  end
+
+  stage_three do
+    say_wizard "recipe stage three"
+    repo = 'https://raw.github.com/RailsApps/rails-mailinglist-signup/master/'
+
+    # >-------------------------------[ Config ]---------------------------------<
+
+    copy_from_repo 'config/initializers/active_job.rb', :repo => repo
+
+    # >-------------------------------[ Models ]--------------------------------<
+
+    copy_from_repo 'app/models/visitor.rb', :repo => repo
+
+    # >-------------------------------[ Controllers ]--------------------------<
+
+    copy_from_repo 'app/controllers/visitors_controller.rb', :repo => repo
+
+    # >-------------------------------[ Jobs ]---------------------------------<
+
+    copy_from_repo 'app/jobs/mailing_list_signup_job.rb', :repo => repo
+
+    # >-------------------------------[ Views ]--------------------------------<
+
+    copy_from_repo 'app/views/visitors/new.html.erb', :repo => repo
+
+    # >-------------------------------[ Routes ]-------------------------------<
+
+    inject_into_file 'config/routes.rb', "  root to: 'visitors#new'\n", :after => "routes.draw do\n"
+    route = '  resources :visitors, only: [:new, :create]'
+    inject_into_file 'config/routes.rb', route + "\n", :after => "routes.draw do\n"
+
+    # >-------------------------------[ Tests ]--------------------------------<
+
+    ### tests not implemented
+
+  end
+end
+# >------------------- recipes/rails_mailinglist_signup.rb -------------------end<
 # >-------------------------- templates/recipe.erb ---------------------------end<
 
 # >-------------------------- templates/recipe.erb ---------------------------start<
