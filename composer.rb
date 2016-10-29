c8c4a1fa3447c6133773088f6510c36a09f47d0a
Daniel Kehoe <daniel@danielkehoe.com>
2016-10-29 17:54:46 +0800

    build using rails_apps_composer 3.1.13

--- composer.rb
+++ composer.rb
@@ -464,7 +464,7 @@ if prefer :apps4, 'learn-rails'
   prefs[:dev_webserver] = 'webrick'
   prefs[:email] = 'sendgrid'
   prefs[:form_builder] = 'simple_form'
-  prefs[:frontend] = 'foundation5'
+  prefs[:frontend] = 'bootstrap3'
   prefs[:github] = false
   prefs[:git] = true
   prefs[:local_env_file] = 'none'
@@ -484,7 +484,6 @@ if prefer :apps4, 'learn-rails'
   add_gem 'gibbon'
   gsub_file 'Gemfile', /gem 'sqlite3'\n/, ''
   add_gem 'sqlite3', :group => :development
-  add_gem 'rails_12factor', :group => :production
 
   stage_three do
     say_wizard "recipe stage three"
@@ -521,7 +520,11 @@ if prefer :apps4, 'learn-rails'
 
     # >-------------------------------[ Assets ]--------------------------------<
 
-    copy_from_repo 'app/assets/javascripts/segmentio.js', :repo => repo
+    copy_from_repo 'app/assets/javascripts/segment.js', :repo => repo
+
+    # >-------------------------------[ Tests ]--------------------------------<
+
+    copy_from_repo 'test/test_helper.rb', :repo => repo
 
   end
 end
